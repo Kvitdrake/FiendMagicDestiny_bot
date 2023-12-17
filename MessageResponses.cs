92a3b3866c35e4282aa4e392d47751cc72dd4517
Kvitdrake <142722843+Kvitdrake@users.noreply.github.com>
2023-12-18 00:36:13 +0300

    Add files via upload

---
+++ MessageResponses.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiendMagicDestiny_bot
+{
+     class MessageResponses
+    {
+        public static KeyboardButton Add => new KeyboardButton("Добавить новое предназначение");
+        public static KeyboardButton Edit => new KeyboardButton("Изменить");
+        public static KeyboardButton Save => new KeyboardButton("Сохранить");
+        public static KeyboardButton NotSave => new KeyboardButton("Не сохранять");
+
+        public static KeyboardButton Back => new KeyboardButton("Назад");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using Microsoft.VisualBasic;
+global using System.IO;
+global using System.Runtime.InteropServices.ComTypes;
+global using System.Security.Claims;
+global using System.Threading;
+global using Telegram.Bot;
+global using Telegram.Bot.Exceptions;
+global using Telegram.Bot.Polling;
+global using Telegram.Bot.Types;
+global using Telegram.Bot.Types.Enums;
+global using Telegram.Bot.Types.ReplyMarkups;
+global using Telegram.Bot.Args;
+global using System.Data.Common;
+global using System.Collections;
+global using Telegram.Bots.Http;
+using Telegram.Bots;
+
+namespace Fiend.Magic_bot
+{
+    class Program
+    {
+        public static void Main()
+        {
+            var client = new TelegramBotClient("5900251255:AAFBjbPWnvf3BgEHKh58XdWx0Thxsj9Z_fQ");
+            client.StartReceiving(Update, Error);
+            Console.ReadLine();
+        }
+
+        async static Task Update(ITelegramBotClient botClient, Update update, CancellationToken token)
+        {
+            var message = update.Message;
+            var chatId = message.Chat.Id;
+            if (update.Type == UpdateType.Message)
+            {
+                //if() короче, завтра пропроси Машу написать с разнх акков сообщения, узнай чат-ид её. 
+                await botClient.SendTextMessageAsync(message.Chat.Id, "Твой персональный помощник для рассчёта предназначения. \r\t Введи отдельными сообщениями сначала имя, дату рождения, тг для связи, а потом просто через пробел все арканы человека.");
+                //сообщение, которое придет послдним превращаешь в строку, строку делишь по символу пробела после чего конвертируешь части 
+                return;
+            }
+
+        }
+
+        private static Task Error(ITelegramBotClient botClient, Exception exception, CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
