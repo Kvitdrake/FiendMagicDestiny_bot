ca660fe873b8a959a159a7ffd4ad01e8c0e04197
Kvitdrake <142722843+Kvitdrake@users.noreply.github.com>
2024-01-02 21:50:26 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -98,10 +98,24 @@ namespace Fiend.Magic_bot
                                 break;
                             case State.Add:
                                 _stateMachine.SaveAddition(chatId, message.Text);
-                                await botClient.SendTextMessageAsync(message.Chat.Id, "Всё готово, лови файл)");
                                 _stateMachine.SendAddition(botClient, chatId);
+
+                                var replyKeyboardMarkup2 = new ReplyKeyboardMarkup(new[]
+                                   {
+                                    new KeyboardButton[]
+                                        {
+                                            MessageResponses.Add,
+                                            MessageResponses.Test
+                                        }
+                                    });
+
+                                await botClient.SendTextMessageAsync(message.Chat.Id, "Всё готово, лови файл)", replyMarkup: replyKeyboardMarkup2);
                                 _stateMachine.ResetState(chatId);
+                                if(message.Text == "Тестовый режим")
+                                    _stateMachine.SetState(chatId, State.Gender);
+                                else
                                 _stateMachine.SetState(chatId, State.None);
+                                
                                 break;
 
                         }
